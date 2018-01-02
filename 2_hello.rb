3e42189a861275fd39c3473b16ee9d7103836583
MJ <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 09:28:41 -0800

    Create 2_hello.rb

---
+++ 2_hello.rb
@@ -0,0 +1,9 @@
+#prints hello world to the console
+puts("Hello world!")
+puts("one", "two", "three")
+
+#the answer to life, the universe and everything
+puts("The answer is")
+puts(42)
+
+puts("ruby programming is cool")
