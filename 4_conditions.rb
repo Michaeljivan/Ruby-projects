f157d214c89ef52d349787c82fe1acae283ded00
MJ <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 09:42:11 -0800

    Update 4_conditions.rb

--- 4_conditions.rb
+++ 4_conditions.rb
@@ -12,11 +12,21 @@ else
   puts("Number is bigger than 10")
 end
 
-#more conditions
+#condition: && 'AND'
 puts("Input a number!")
 number = gets.to_i
 if (number > 10) && (number < 20)
   puts("Your number was greater than ten AND less than twenty!")
 else
   puts("Your number was either less than ten, OR greater than twenty!")
+end
+
+#condition: || 'OR'
+puts("Enter a letter")
+letter = gets.to_s
+
+if ((letter == "A") || (letter == "B"))
+  puts("Thats a magical letter")
+else
+  puts("Sorry, that letter is not magical")
 end
