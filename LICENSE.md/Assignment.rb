009c1c7a91e1d8ed26e691fbb354465c0b16ec38
MJ <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 09:24:21 -0800

    Create Assignment.rb

---
+++ LICENSE.md/Assignment.rb
@@ -0,0 +1,22 @@
+#Assignment
+cool_thing = "racecars"
+puts(cool_thing)
+
+#Re-assignment
+cool_thing = "lasers"
+puts(cool_thing)
+
+
+#get user input
+puts("type your name!")
+name = gets()
+puts(name)
+
+#integer to string
+one = 1.to_s
+two = 2.to_s
+
+#chomp, read line and strip off the enter character at the end.
+puts("Type in you name!")
+name = gets.chomp
+puts("Hello " + name + "!")
