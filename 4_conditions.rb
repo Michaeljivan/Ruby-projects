bc3670e1ee22c4bd25f2b7aa550da205c35e7ee8
MJ <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 09:36:00 -0800

    Create 4_conditions.rb

---
+++ 4_conditions.rb
@@ -0,0 +1,23 @@
+#conditional if statement
+puts("Type in a number")
+number = gets.to_i
+
+if number < 10
+  puts("Number is smaller than 10")
+end
+
+if number < 10
+  puts("Number is small")
+else
+  puts("Number is bigger than 10")
+end
+
+
+#more conditions
+puts("Input a number!")
+number = gets.to_i
+if (number > 10) && (number < 20)
+  puts("Your number was greater than ten AND less than twenty!")
+else
+  puts("Your number was either less than ten, OR greater than twenty!")
+end
