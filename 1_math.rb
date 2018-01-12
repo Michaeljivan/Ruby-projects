9ab207477214a9571b0ea8f3d47b98d2d98425b5
Michael Jivan <32310825+Michaeljivan@users.noreply.github.com>
2018-01-12 11:41:29 -0800

    Update 1_math.rb

--- 1_math.rb
+++ 1_math.rb
@@ -15,19 +15,4 @@ puts(4 != 5)
 #string to integer
 seven = "4".to_i + "3".to_i
 puts(seven)
-redskins 	-not a playoff team
-chiefs		-playoff team and LOST
-giants 		-not a playoff team
-chargers	-not a playoff team
-cardnials	-not a playoff team
-panthers	-playoff team and WON
-redskins	-not a playoff team 
-49ers		-not a playoff team
-broncos		-not a playoff team
-cowboys		-not a playoff team
-bears		-not a playoff team
-seahawks	-not a playoff team and LOST
-rams		-playoff team and WON
-giants		-not a playoff team
-raiders		-not a playoff team
-cowboys		-not a playoff team
+
